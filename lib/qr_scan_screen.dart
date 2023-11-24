6b4ee74eca008079a3d06a0c03e0a6e31742f875
Ang Wei Long <weilong02@gmail.com>
2023-11-24 16:50:50 +0800

    add qr scan screen

---
+++ lib/qr_scan_screen.dart
@@ -0,0 +1,87 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QRScanScreen extends StatefulWidget {
+  const QRScanScreen({super.key, required String title});
+
+  @override
+  State<StatefulWidget> createState() => _QRScanScreenState();
+}
+
+class _QRScanScreenState extends State<QRScanScreen> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller; // Nullable controller
+  Barcode? result; // Nullable result
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller?.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller?.resumeCamera();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scan QR Code'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 4,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+              overlay: QrScannerOverlayShape(
+                borderColor: Colors.green,
+                borderRadius: 10,
+                borderLength: 30,
+                borderWidth: 10,
+                cutOutSize: MediaQuery.of(context).size.width * 0.8,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: (result != null)
+                  ? Text('Barcode Type: ${result!.format}   Data: ${result!.code}')
+                  : const Text('Scan a code'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+      if (result != null && result!.code != null) {
+        controller.pauseCamera();
+        _navigateToNextScreen(context, result!.code!); // Ensure the code is not null before navigating
+      }
+    });
+  }
+
+  void _navigateToNextScreen(BuildContext context, String scanResult) {
+    // Use the Navigator to push to the CaptureItemsScreen or any other screen you need
+    // Navigator.pushNamed(context, '/captureItems', arguments: scanResult);
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
