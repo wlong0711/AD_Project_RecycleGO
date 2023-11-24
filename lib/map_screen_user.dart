9944a06e6d90a3b5a73642f8fcfa757c590a57fc
Alvinleow <alvinleowyankai@gmail.com>
2023-11-24 23:56:53 +0800

    Map (User View)

---
+++ lib/map_screen_user.dart
@@ -0,0 +1,214 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
+class MapScreenUser extends StatefulWidget {
+  const MapScreenUser({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreenUser> {
+  GoogleMapController? mapController;
+  TextEditingController _searchController = TextEditingController();
+  LatLng _initialPosition = const LatLng(0.0, 0.0);
+  Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition();
+    _loadDropPoints();
+  }
+
+  Future<void> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When permissions are granted, get the current position
+    Position position = await Geolocator.getCurrentPosition();
+    setState(() {
+      _initialPosition = LatLng(position.latitude, position.longitude);
+    });
+
+    mapController?.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        target: _initialPosition,
+        zoom: 18.0,
+      ),
+    ));
+  }
+
+  void _loadDropPoints() {
+  FirebaseFirestore.instance.collection('drop_points').snapshots().listen((snapshot) {
+    setState(() {
+      _markers.clear();
+      for (var doc in snapshot.docs) {
+        Map<String, dynamic> pointData = doc.data();
+        LatLng point = LatLng(pointData['latitude'], pointData['longitude']);
+        _markers.add(Marker(
+          markerId: MarkerId(doc.id),
+          position: point,
+          infoWindow: InfoWindow(
+            title: pointData['title'],
+            snippet: pointData['address'],
+            onTap: () {
+              _showDropPointDetails(pointData);
+            }
+          ),
+        ));
+      }
+    });
+  });
+}
+
+  void _showDropPointDetails(Map<String, dynamic> pointData) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(pointData['title'], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+        content: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Address:',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.blue),
+              ),
+              Text(pointData['address'] ?? 'Not available', style: TextStyle(fontSize: 14)),
+              SizedBox(height: 10),
+              Divider(),
+              Text(
+                'Operating Hours:',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.blue),
+              ),
+              Text(pointData['operationHours'] ?? 'Not available', style: TextStyle(fontSize: 14)),
+              SizedBox(height: 10),
+              Divider(),
+              Text(
+                'Recyclable Items:',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.blue),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: (pointData['recycleItems'] as List<dynamic>).map((item) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0),
+                    child: Text(item.toString(), style: TextStyle(fontSize: 14)),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Close', style: TextStyle(color: Colors.blue)),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  Future<void> _searchAndNavigate() async {
+    if (_searchController.text.isEmpty) return;
+    try {
+      List<Location> locations = await locationFromAddress(_searchController.text);
+      if (locations.isNotEmpty) {
+        mapController?.animateCamera(CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(locations.first.latitude, locations.first.longitude),
+            zoom: 14.0,
+          ),
+        ));
+      }
+    } catch (e) {
+      // Handle error or no result
+      print('Error: ${e.toString()}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: TextField(
+            controller: _searchController,
+            onSubmitted: (value) => _searchAndNavigate(),
+            decoration: InputDecoration(
+              icon: Icon(Icons.search, color: Colors.black),
+              hintText: 'Enter location name',
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.blue,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.clear),
+            onPressed: () {
+              _searchController.clear();
+            },
+          ),
+        ],
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _initialPosition,
+          zoom: 18.0,
+        ),
+        markers: _markers,
+        myLocationEnabled: true,
+        zoomControlsEnabled: true,
+        mapType: MapType.normal,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+      floatingActionButton: Padding(
+      padding: const EdgeInsets.only(top: 180.0, right: 0.0),
+      
+    ),
+    );
+  }
+}
+
+
