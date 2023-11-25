4f2c3e4b7c6b7c9000120be31555b0f813e41e1c
Alvinleow <alvinleowyankai@gmail.com>
2023-11-25 14:58:05 +0800

    Different Interface for User View & Admin View

--- lib/Shared Pages/StartUp Pages/home_page.dart
+++ lib/Shared Pages/StartUp Pages/home_page.dart
@@ -1,7 +1,7 @@
-// home_page.dart
 import 'package:flutter/material.dart';
 import 'package:recycle_go/Shared%20Pages/QR%20Scan%20&%20Upload%20Page/qr_scan_screen.dart';
 import 'package:recycle_go/Admin%20Only%20Pages/verify_reward.dart';
+import 'package:recycle_go/models/global_user.dart';
 import '../../Admin Only Pages/map_screen_admin.dart';
 import '../../User Only Pages/map_screen_user.dart';
 
@@ -18,45 +18,71 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MapScreenAdmin(title:'Admin View Map')),
-                );
-              },
-              child: const Text('Map for Admin'),
+            Text('Welcome, ${GlobalUser.userName ?? 'User'}!',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MapScreenUser(title:'User View Map')),
-                );
-              },
-              child: const Text('Map for User'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const VerifyRewardPage()),
-                );
-              },
-              child: const Text('Rewards'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const QRScanScreen(title:'Scan QR')),
-                );
-              },
-              child: const Text('Scan QR'),
-            ),
+            SizedBox(height: 20), // Spacing after the welcome text
+
+            // Common Buttons
+            _commonButtons(context),
+
+            // Admin-only Buttons
+            if (GlobalUser.userLevel == 1) 
+              _adminOnlyButtons(context),
+
           ],
         ),
       ),
+    );
+  }
+
+  Widget _adminOnlyButtons(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MapScreenAdmin(title: 'Admin View Map')),
+            );
+          },
+          child: const Text('Map for Admin'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const VerifyRewardPage()),
+            );
+          },
+          child: const Text('Verify for Rewards'),
+        ),
+      ],
+    );
+  }
+
+  Widget _commonButtons(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MapScreenUser(title: 'User View Map')),
+            );
+          },
+          child: const Text('Map'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const QRScanScreen(title: 'Scan QR')),
+            );
+          },
+          child: const Text('Scan QR'),
+        ),
+      ],
     );
   }
 }
