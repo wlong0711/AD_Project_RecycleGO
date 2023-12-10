98d8ec9694985c2db713f16860342277eb136cab
BrianT5702 <chin-@graduate.utm.my>
2023-12-10 13:15:58 +0800

    Changed the view in the homepage

--- lib/Shared Pages/StartUp Pages/home_page.dart
+++ lib/Shared Pages/StartUp Pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:recycle_go/Admin%20Only%20Pages/view_report_issues.dart';
 import 'package:recycle_go/Shared%20Pages/QR%20Scan%20&%20Upload%20Page/qr_scan_screen.dart';
 import 'package:recycle_go/Admin%20Only%20Pages/verify_reward.dart';
@@ -16,27 +17,100 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text('Welcome, ${GlobalUser.userName ?? 'User'}!',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20), // Spacing after the welcome text
-
-            // Common Buttons
-            _commonButtons(context),
-
-            // Admin-only Buttons
-            if (GlobalUser.userLevel == 1) 
-              _adminOnlyButtons(context),
-
-          ],
-        ),
+      body: ListView(
+        children: <Widget>[
+          _buildTopCarousel(),
+          _buildWelcomeText(),
+          _buildGridMenu(context),
+          // _buildNewsFeed(),
+        ],
       ),
     );
   }
+
+  Widget _buildTopCarousel() {
+    // Placeholder for carousel images
+    final List<String> imgList = [
+      'assets/image1.jpg',
+      'assets/image2.jpg',
+      // Add more images
+    ];
+
+    return CarouselSlider(
+      options: CarouselOptions(
+        autoPlay: true,
+        aspectRatio: 2.0,
+        enlargeCenterPage: true,
+      ),
+
+      items: imgList.map((item) => Container(
+        child: Center(
+          child: Image.asset(item, fit: BoxFit.cover, width: 1000)
+        ),
+      )).toList(),
+    );
+  }
+
+  Widget _buildWelcomeText() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Text(
+        'Welcome, ${GlobalUser.userName ?? 'User'}!',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Widget _buildGridMenu(BuildContext context) {
+    return GridView.count(
+    crossAxisCount: 2,
+    shrinkWrap: true, // Important to prevent infinite height error
+    physics: NeverScrollableScrollPhysics(), // to disable GridView's scrolling
+    children: <Widget>[
+      _buildMenuButton(context, Icons.map, 'Map', () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const MapScreenUser(title: 'User View Map')),
+        );
+      }),
+      _buildMenuButton(context, Icons.qr_code_scanner, 'Scan QR', () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const QRScanScreen(title: 'Scan QR')),
+        );
+      }),
+      _buildMenuButton(context, Icons.report_problem, 'Report Issue', () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const ReportIssueScreen(title: 'Report an Issue')),
+        );
+      }),
+      // Add more buttons as needed
+    ],
+  );
+}
+
+Widget _buildMenuButton(BuildContext context, IconData icon, String label, VoidCallback onPressed) {
+  return Card(
+    child: InkWell(
+      onTap: onPressed,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 50.0),
+          Text(label, style: TextStyle(fontSize: 16.0))
+        ],
+      ),
+    ),
+  );
+}
+
+  // Widget _buildNewsFeed() {
+  //   // Fetch news articles or posts from a database or API
+  //   // Display them here as a list or cards
+  // }
 
   Widget _adminOnlyButtons(BuildContext context) {
   return Column(
