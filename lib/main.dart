8b72036353b44d58d08109452783ad6d3e0d0346
Alvinleow <alvinleowyankai@gmail.com>
2023-12-30 00:11:54 +0800

    Set CompanyLogo as Pre-load Object

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,45 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:recycle_go/Shared%20Pages/StartUp%20Pages/welcome_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:recycle_go/models/company_logo.dart';
 import 'firebase_options.dart';
 
-Future main() async{
-  WidgetsFlutterBinding.ensureInitialized(); // Ensure plugin services are initialized
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+
+  // Initialize the company logo here
+  final CompanyLogo companyLogo = CompanyLogo(
+    'https://firebasestorage.googleapis.com/v0/b/recyclego-64b10.appspot.com/o/Company%20Logo%2FLogo.png?alt=media&token=d70a3db8-c0c0-4849-9aee-7ec04c4bbbd8', // Replace with your actual image URL
+  );
+
+  await companyLogo.preloadImage(); 
+
+  runApp(MyApp(companyLogo: companyLogo));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final CompanyLogo companyLogo;
 
-  // This widget is the root of your application.
+  const MyApp({super.key, required this.companyLogo});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'RecycleGo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    // Use Provider to make CompanyLogo available down the widget tree
+    return Provider<CompanyLogo>(
+      create: (_) => companyLogo,
+      child: MaterialApp(
+        title: 'RecycleGo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const WelcomePage(), // WelcomePage should be modified to use Provider if it needs CompanyLogo
       ),
-
-      home: const WelcomePage(),
     );
   }
 }
